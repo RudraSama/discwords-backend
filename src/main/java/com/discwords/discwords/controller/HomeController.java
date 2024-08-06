d76426984f63b7ed2dee0a93af50839aa4f99048
Lucky <luckybairagi96@gmail.com>
2024-08-07 03:47:06 +0530

    First commit

---
+++ src/main/java/com/discwords/discwords/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.discwords.discwords.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String greet(){
+
+        return "Hii";
+    }
+}
