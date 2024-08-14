306878deaf2f16193138c765a0b9e778417b0d8c
Lucky <luckybairagi96@gmail.com>
2024-08-14 14:56:41 +0530

    Added fetch direct messages

--- src/main/java/com/discwords/discwords/service/DirectMessageService.java
+++ src/main/java/com/discwords/discwords/service/DirectMessageService.java
@@ -2,11 +2,15 @@ package com.discwords.discwords.service;
 
 
 import com.discwords.discwords.model.ConversationDTO;
+import com.discwords.discwords.model.DirectMessageDTO;
+
+import java.util.List;
 
 public interface DirectMessageService {
 
     public ConversationDTO createConversation(long profile_id_one, long profile_id_two);
     public ConversationDTO getConversation(long conversation_id);
     public void sendMessage(long conversation_id, long profile_id, String message);
+    public List<DirectMessageDTO> getMessages(long conversation_id);
 
 }
