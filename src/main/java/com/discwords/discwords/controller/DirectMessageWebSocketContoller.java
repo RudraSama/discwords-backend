36905fbf20c0e5cd154b93364c297042cad62083
Lucky <luckybairagi96@gmail.com>
2024-09-04 02:09:11 +0530

    new commit

--- src/main/java/com/discwords/discwords/controller/DirectMessageWebSocketContoller.java
+++ src/main/java/com/discwords/discwords/controller/DirectMessageWebSocketContoller.java
@@ -2,9 +2,11 @@ package com.discwords.discwords.controller;
 
 import com.discwords.discwords.model.DirectMessage;
 import com.discwords.discwords.model.DirectMessageDTO;
+import com.discwords.discwords.service.DirectChatServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -14,7 +16,6 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class DirectMessageWebSocketContoller {
 
-<<<<<<< HEAD:src/main/java/com/discwords/discwords/controller/DirectMessageWebSocketController.java
     @Autowired
     private DirectChatServiceImpl directChatService;
 
@@ -25,16 +26,5 @@ public class DirectMessageWebSocketContoller {
         return messageDTO;
 
     }
-=======
 
-    @Autowired
-    private SimpMessagingTemplate simpMessagingTemplate;
-
-//    @MessageMapping("/chat.sendMessage")
-//    public void sendMessage(ChatMessage chatMessage){
-//        System.out.println(chatMessage.getConversation_id());
-//        simpMessagingTemplate.convertAndSend("/topic/"+chatMessage.getConversation_id(), chatMessage);
-//
-//    }
->>>>>>> parent of 941e199 (kuch bhi):src/main/java/com/discwords/discwords/controller/DirectMessageWebSocketContoller.java
 }
